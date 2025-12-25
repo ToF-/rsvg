3b923a34336b56a2f004c7f18fd808b8fda445d3
ToF- <cthibauttof@gmail.com>
2025-12-25 19:20:20 +0100

    shapegrid wip

---
+++ src/shapegrid/main.rs
@@ -0,0 +1,83 @@
+use std::f64::consts::PI;
+use svg::Document;
+use svg::node::element::Path;
+use svg::node::element::path::Data;
+use svg::node::element::Line;
+
+const MAX_EDGES: usize = 3;
+const MAX_STEPS: usize = 16;
+
+type Point = (f64, f64);
+
+
+pub fn shape(x0: f64, y0: f64, length: f64, start_angle: f64) -> Vec<Vec<Point>> {
+    let mut points: Vec<Vec<Point>> = vec![];
+    let mut alpha: f64 = start_angle;
+    const THETA: f64 = (2.0 * PI) / MAX_EDGES as f64;
+    for _ in 0..MAX_EDGES {
+        let pt = alpha.sin_cos();
+        let mut pts: Vec<Point> = vec![];
+        let mut l: f64 = 0.0;
+        for _ in 0..MAX_STEPS {
+            let beta = alpha + PI - THETA;
+            let qt = beta.sin_cos();
+            let mut x = x0 + length * pt.1 + l * qt.1;
+            let mut y = y0 + length * pt.0 + l * qt.0;
+            pts.push((x, y));
+            l += length / MAX_STEPS as f64;
+        }
+        alpha += THETA;
+        points.push(pts);
+
+    }
+    points
+}
+
+pub fn line_path(points: Vec<Point>, color: &str) -> Path {
+    let mut data = Data::new()
+        .move_to(points[0]);
+    for i in 1..points.len() {
+        data = data.line_to(points[i]);
+    }
+    data = data.line_to(points[0]);
+    data = data.close();
+    Path::new()
+        .set("fill", "none")
+        .set("stroke", color)
+        .set("stroke-width", 1)
+        .set("d", data)
+}
+
+pub fn line(a: Point, b: Point, color: &str) -> Line {
+    Line::new()
+        .set("x1", a.0)
+        .set("y1", a.1)
+        .set("x2", b.0)
+        .set("y2", b.1)
+        .set("stroke", color)
+}
+
+
+fn main() {
+    let x0 = 500.0;
+    let y0 = 500.0;
+    let length = 400.0;
+    const START_ANGLE: f64 = 0.0;
+    let shape = shape(x0, y0, length, START_ANGLE);
+    let mut document = Document::new()
+        .set("viewBox", (0, 0, 1000, 1000));
+    let mut global: Vec<Point> = vec![];
+    for i in 0..MAX_EDGES {
+        global.push(shape[i][0]);
+    }
+    document = document.add(line_path(global, "green"));
+    for i in 0..MAX_EDGES {
+        for j in 0..MAX_STEPS {
+            for k in 0..MAX_STEPS {
+            document = document.add(line(shape[i][j],shape[(i+k)%MAX_EDGES][(MAX_STEPS-j)%MAX_STEPS], "blue"));
+        }
+        }
+    }
+    svg::save("images/shapegrid.svg", &document).unwrap();
+}
+
